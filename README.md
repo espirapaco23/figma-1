21e5c0612807e6b9bdf9fe874580b700bcaa0d0b
Igor Nefedov <102536675+espirapaco23@users.noreply.github.com>
2023-02-17 09:39:52 +0300

    no media queries

---
+++ README.md
@@ -0,0 +1,8 @@
+### E-Commerce Website
+
+- E-Commerce Website Using HTML CSS JavaScript.
+- N0 Compatible with all mobile devices.
+- https://www.figma.com/community/file/1109527751943550595 - Figma
+- https://boxicons.com/ - Icons.
+
+![preview img](/preview.png)
---
+++ script.js
@@ -0,0 +1,28 @@
+let navMenu = document.getElementById("nav-menu");
+let navToggle = document.getElementById("nav-cart");
+let navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+let navLink = document.querySelectorAll(".nav__link");
+function linkAction() {
+  let navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+function scrollHeader() {
+  let header = document.getElementById("header");
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
